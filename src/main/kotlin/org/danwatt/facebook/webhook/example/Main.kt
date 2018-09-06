19b2f8ebe1a4b51031d6f9740ad15163f03a3b99
Dan Watt <daniel.watt@daveramsey.com>
2018-09-06 14:40:21 -0500

    Progress

--- src/main/kotlin/org/danwatt/facebook/webhook/example/Main.kt
+++ src/main/kotlin/org/danwatt/facebook/webhook/example/Main.kt
@@ -9,11 +9,12 @@ import org.jooby.run
 class Main : Kooby({
     use(Jackson().doWith { it.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE })
 
-    /*put("/user-profile/:id") { request ->
-        val id = request.param("id").value
-        val body = request.body(Profile::class.java)
-         service.save(id, body)
-    }*/
+    //Facebook requires a privacy policy page
+    get("/privacy") { request, response ->
+        response.status(200).type("text/html")
+                .send("<p>We dont want your data. We discard it, as this is just a test</p>")
+    }
+    //Verification
     get("/webhook") { request, response ->
         val mode = request.param("hub.mode").value()
         val challenge = request.param("hub.challenge").value()
@@ -21,18 +22,15 @@ class Main : Kooby({
 
         response.status(200).type("text/plain").send(challenge)
     }
-    post ("/webhook") {request,response ->
+
+    post("/webhook") { request, response ->
         println(request.body().value())
+        val body = request.body().value()
+        //TODO: Decode
+        response.status(200)
     }
     get("/test") { request, response ->
         response.status(200).type("application/json").send(mapOf("test" to "ok"))
-        // val id = request.param("id").value
-        //val found = service.get(id)
-        /* when {
-             found.isPresent -> response.status(200).type("application/json").send(found.get())
-             else -> response.status(404)
-         }
-         */
     }
 })
 
